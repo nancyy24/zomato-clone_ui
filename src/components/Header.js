3ae1caeb65cf68803ce94cfc307b608e7488dc93
Nancy <guptanancy2403@gmail.com>
2022-10-14 03:11:52 +0530

    add

---
+++ src/components/Header.js
@@ -0,0 +1,124 @@
+import { useNavigate } from "react-router-dom";
+import { GoogleOAuthProvider} from "@react-oauth/google";
+import {GoogleLogin} from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+function Header(props){
+  let navigate = useNavigate();
+
+let getTokenDetails = () => {
+  let token = localStorage.getItem("auth-token")
+  if(token === null)
+  {
+    return false;
+  }
+  else{
+    return jwt_decode(token);
+  }
+}
+
+let [userLogin,setUserLogin] = useState(getTokenDetails());
+
+  let onSuccess = (credentialResponse)=>
+  {
+    let token = credentialResponse.credential;
+    let data = jwt_decode(token);
+    // console.log(data);
+    // save the data
+    localStorage.setItem("auth-token",token);
+    Swal.fire({
+      icon: 'success',
+      title: 'Login Successfully ',
+      text: 'Welcome to Zomato!',
+    })
+    .then(()=>
+    { 
+      window.location.reload();
+
+    })
+    
+    // alert('user login successfully');
+  };
+
+  
+  let onError =() =>{
+    // alert("Login fail");
+    Swal.fire({
+      icon: 'error',
+      title: 'Login Failed!!',
+      text: 'Something went wrong!',
+      footer: '<a href="">Why do I have this issue?</a>'
+    })
+  };
+
+  let logout =() =>
+  {
+    // remove the adta from the local storage
+    localStorage.removeItem("auth-token");
+    // alert("User Logout Successfully");
+    Swal.fire({
+      icon: 'success',
+      title: 'User Logout Successfully ',
+      text: 'LogOut to Zomato Site!',
+    })
+    .then(()=>
+    { 
+      setUserLogin(false);
+      window.location.reload();
+
+    })
+    
+    
+   
+
+  }
+    return (<>
+
+
+    
+<GoogleOAuthProvider clientId ="67689682803-lk4a458n1sgcidj63m0bf5l92mmslbgv.apps.googleusercontent.com">
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <GoogleLogin onSuccess ={onSuccess}
+        onerror={onError}
+        
+        />;
+      </div>
+     
+    </div>
+  </div>
+</div>
+         <header className="row">
+      <div className={"col-12  p-2 " + props.color}>
+        <div className="container-fluid d-flex justify-content-between align-items-center">
+          <p className="m-0 logo d-flex justify-content-center cursor-pointer align-items-center fw-bold fs-4" onClick={() =>{
+            navigate("/")
+          }}>
+            e!
+          </p>
+          { userLogin ? ( <div className="d-flex">
+            <p className="mt-2 me-3 fw-bold text-white">Welcome, {userLogin.given_name}</p>
+            <button className="btn btn-outline-light" onClick={logout}>Logout</button>
+          </div>) : (
+            <div>
+            <button className="btn text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</button>
+            <button className="btn btn-outline-light">Create An account</button>
+          </div>
+          )}
+           
+        </div>
+      </div>
+    </header>
+</GoogleOAuthProvider>
+    </>);
+}
+
+export default Header;
---
+++ src/components/home/WallPaper.js
@@ -0,0 +1,164 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "../../css/homePage.css";
+import Header from "../Header";
+import Swal from "sweetalert2";
+
+function WallPaper(){
+
+    let [locationList,setLocationList] = useState([]);
+
+    let [disable,setDisable] = useState(true);
+
+    let getLocationList = async()=>{
+    try{
+        let response = await axios.get("http://localhost:5000/api/get-location")
+        let data =response.data;
+        // console.log(data);
+        if(data.status === true){
+            setLocationList([...data.result]);
+        }
+        else{
+            setLocationList([]);
+        }
+    }
+    catch(error){
+        console.log(error);
+        // alert("server error");
+        Swal.fire({
+            icon: 'error',
+            title: 'Server Error',
+            text: 'Something went wrong!',
+          })
+    }
+    }
+
+    let getLocationId = async (event) =>{
+        let value_data = event.target.value;
+        // console.log(value_data);
+        if(value_data !== "")
+        {   try{
+            let response =  await axios.get(`http://localhost:5000/api/get-restaurant-by-location-id/${value_data}`);
+            let data = response.data;
+            console.log(data);
+            console.log(data.result);
+
+            if(data.result.length === 0)
+            {
+                setDisable(true);
+            }
+            else{
+                setDisable(false);
+            }
+        }
+        catch(error)
+        {    
+            // alert("server error");
+            Swal.fire({
+                icon: 'error',
+                title: 'Server Error',
+                text: 'Something went wrong!',
+              })
+        }
+
+        }
+        else{
+            setDisable(true);
+        }
+    }
+    useEffect(() => {
+        getLocationList();
+    },[]);
+
+
+
+    return <>
+    
+         
+
+         <section className="row backimg">
+            {/* <header className="col-12 py-2">
+                <div className="container d-flex justify-content-end">
+                    <button className="btn text-white me-3 d-none d-sm-block" data-bs-toggle="modal"
+                        data-bs-target="#staticBackdrop">Login</button>
+                    <button className="btn btn-outline-light me-5 d-none d-sm-block">
+                        Create an account
+                    </button>
+                </div>
+            </header> */}
+<div className="col-12">  <Header color="" /></div>
+           
+          <div className="row">
+            <div className="col-12 d-flex justify-content-center my-2">
+                <p className="m-0 logo_main text-danger h1 fw-bold">e!</p>
+            </div>
+            <div className="col-12 d-flex justify-content-center my-2">
+                <p className="m-0 text-white h2 text-center">
+                    Find the best restaurants, cafés, and bars
+                </p>
+            </div>
+            <div className=" d-flex justify-content-center my-2 mb-5">
+                    <div className=" col-12 col-sm-3 p-sm-2 mb-2 ">
+                        <div className="set-location me-3">
+                            {/* <input placeholder="Please type a location" className="form-control p-3 p-sm-2 " /> */}
+                            <select className="form-control p-3 p-sm-2 " onChange={getLocationId} >
+                            <option value=""> Please select a location</option>
+                            { locationList.map((location,index) => {
+                            return <> <option value={location.location_id} key={index}>{location.name},{location.city}</option> </>
+                                
+                            })}
+                            </select>
+                            {/* <!-- <ul className="set-location-list text-muted small">
+                                <li className="set-bottom p-1">Sarjapur Road, Bengaluru </li>
+                                <li className="set-bottom p-1">HSR Layout, Bengaluru</li>
+                                <li className="set-bottom p-1">Kormangala, Bengaluru</li>
+                                <li className="p-1">Jay Nagar, Bengaluru</li>
+                            </ul> --> */}
+                        </div>
+                    </div>
+                    <div className=" col-12 col-sm-3 p-2">
+                        <div className="set-location ">
+                            <div className="input-group">
+                                <span className="input-group-text"><i className="fa fa-search"></i></span>
+                                <input type="text" className="form-control p-3 p-sm-2" placeholder="Search for restaurants" disabled={disable} />
+                            </div>
+                            {/* <!-- <ul className="set-location-list  small">
+                                <li className="set-bottom p-2">
+                                    <div className="d-flex">
+                                        <img src="/pictures/1.png" className="search-list-img">
+                                        <div className="ms-2">
+                                            <p className="m-0 h6 fw-bold ">The Big Chill Cakery</p>
+                                            <p className="m-0 small text-muted">Sarjapur Road, Bengaluru</p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li className="set-bottom p-2">
+                                    <div className="d-flex">
+                                        <img src="/pictures/1.png" className="search-list-img">
+                                        <div className="ms-2">
+                                            <p className="m-0 h6 fw-bold ">The Big Chill Cakery</p>
+                                            <p className="m-0 small text-muted">Sarjapur Road, Bengaluru</p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li className="p-2">
+                                    <div className="d-flex">
+                                        <img src="/pictures/1.png" className="search-list-img">
+                                        <div className="ms-2">
+                                            <p className="m-0 h6 fw-bold ">The Big Chill Cakery</p>
+                                            <p className="m-0 small text-muted">Sarjapur Road, Bengaluru</p>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul> --> */}
+                        </div>
+                    </div>
+                </div>
+                </div>
+        </section>
+       
+    </>
+}
+
+export default WallPaper;
+
