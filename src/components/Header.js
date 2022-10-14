efd405defa97301a2231cf15d9ff1b7449c1596e
Nancy <guptanancy2403@gmail.com>
2022-10-14 12:03:18 +0530

    changing carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -82,7 +82,7 @@ let [userLogin,setUserLogin] = useState(getTokenDetails());
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Google Sign-In</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
